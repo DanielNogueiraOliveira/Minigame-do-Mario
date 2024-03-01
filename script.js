8132d752527db6ddec5d4a6a64a7559c3a9676cc
DanielNogueiraOliveira <nogueira.dan07@gmail.com>
2024-02-29 23:09:46 -0300

    Game-Over adicionado e fundo estilizado
    foi colocado a função de game-over( quando à colisão do mario com o cano o jogo para) e o fundo foi customizado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const cano = document.querySelector('.cano');
 
 //troca a classe do elemento "mario" para "pulo" quando clica qlqr tecla
 const pulo = () => {
@@ -9,5 +10,28 @@ const pulo = () => {
         mario.classList.remove('pulo');
     }, 500);
 }
+const loop = setInterval(() => {
+    
+
+    const posicaoCano = cano.offsetLeft;
+    const posicaoMario = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    if(posicaoCano <= 110 && posicaoCano > 0 && posicaoMario < 80) {
+    
+        cano.style.animation = 'none';
+        cano.style.left = `${posicaoCano}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${posicaoMario}px`;
+
+        mario.src = '/assents/game-over.png';
+        mario.style.width = "75px";
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop)
+
+    }
+
+}, 10);
 
 document.addEventListener('keydown', pulo);
